96b2459b1dcc3c26142caee0616f2e1de6d3718b
Vladlen <vladlenkaraush@gmail.com>
2019-10-21 09:43:32 +0300

    concat refactor

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,26 +1,29 @@
 import http from "./httpService";
 import { apiEndpoint } from "../config.json";
-import { getGenres } from "./genreService";
 
 const apiUrl = apiEndpoint + "/api/movies";
+
+function getMovieUrl(id) {
+  return `${apiUrl}/${id}`;
+}
 export async function getMovies() {
   return await http.get(apiUrl);
 }
 
 export async function getMovie(id) {
-  console.log("request url: ", apiUrl + "/" + id);
-  return await http.get(apiUrl + "/" + id);
+  console.log("request url: ", getMovieUrl(id));
+  return await http.get(getMovieUrl(id));
 }
 
 export async function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._id;
-    return http.put(apiUrl + "/" + movie._id, body);
+    return http.put(getMovieUrl(movie._id), body);
   }
   return http.post(apiUrl, movie);
 }
 
 export function deleteMovie(id) {
-  return http.delete(apiUrl + "/" + id);
+  return http.delete(getMovieUrl(id));
 }
